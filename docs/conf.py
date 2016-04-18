740ec9a3c2c40c2c35df816033e8a5c541299a72
Pablo Daniel Pareja Obregon <parejaobregon@gmail.com>
2016-04-18 20:53:06 -0300

    Minor fixes.

--- docs/conf.py
+++ docs/conf.py
@@ -102,6 +102,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
+# html_theme = 'default'
+
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from
 # docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
@@ -112,7 +114,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
-
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
